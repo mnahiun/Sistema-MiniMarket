46dc046ec2322874cab06a38d1d04c87bf3ce262
mnahiun <duartemateusnahin701@gmail.com>
2026-04-20 21:18:31 -0500

    feat: Se creo la clase abstracta Persona para los usuarios

---
+++ src/entidades/Persona.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidades;
+
+/**
+ *
+ * @author Nahin
+ */
+public abstract class  Persona {
+    private String nombre;
+    private String cedula;
+
+    public Persona(String nombre, String cedula) {
+        this.nombre = nombre;
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+    
+    public abstract void mostrarInfo();  
+    
+}
