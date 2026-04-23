774941795c632fb14300295458b79775c11b2f50
mnahiun <duartemateusnahin701@gmail.com>
2026-04-22 20:59:28 -0500

    feat: Creamos la clase abstracta Empleado

---
+++ src/entidades/Empleado.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidades;
+
+/**
+ * Clase abstracta Empleado.
+ */
+public abstract class Empleado {
+    
+      private int idEmpleado;
+      private String nombre;
+      private String cedula;
+
+    public Empleado(int idEmpleado, String nombre, String cedula) {
+        this.idEmpleado = idEmpleado;
+        this.nombre = nombre;
+        this.cedula = cedula;
+    }
+
+    public int getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public void setIdEmpleado(int idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+      
+    public String getRol(){
+    
+    }
+       // Método abstracto que define el acceso del empleado (Polimorfismo)
+  public abstract String[] getPermisosAcceso();
+  
+       public abstract void mostrarInfo();  
+}
