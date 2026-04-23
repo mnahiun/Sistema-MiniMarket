4b2ddc92c825b544ac223a4a46fde3500c1bf86d
mnahiun <duartemateusnahin701@gmail.com>
2026-04-22 21:39:20 -0500

    feat: Creamos la clase Bodeguero con sus metodos implementados

---
+++ src/entidades/Bodeguero.java
@@ -0,0 +1,32 @@
+
+package entidades;
+
+/**
+ * Clase Bodeguero.
+ * Aplica: Herencia (extiende Empleado), Polimorfismo
+ * 
+ */
+public class Bodeguero  extends Empleado{
+    
+  public Bodeguero(int idEmpleado, String nombre, String cedula) {
+    super(idEmpleado, nombre, cedula);
+  }
+  
+    @Override
+  public String getRol() {
+  return "Bodeguero";
+  }
+  
+   @Override
+  public String[] getPermisosAcceso() {
+  String[] permisos = {"VER_INVENTARIO", "ACTUALIZAR_INVENTARIO"};
+  return permisos;
+  }
+  
+   @Override
+    public String mostrarInfo() {
+    return "Id empleado: " + getIdEmpleado() + "\n"
+            + "Nombre: " + getNombre() +  "\n"
+            + "Cedula: " + getCedula();
+    }
+}
