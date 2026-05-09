347f7fc8607616c6e8dd87089ae743d3e2134b26
mnahiun <duartemateusnahin701@gmail.com>
2026-05-09 17:44:16 -0500

    feat: Se creo la clase Administrador

--- src/entidades/Administrador.java
+++ src/entidades/Administrador.java
@@ -7,35 +7,37 @@ package entidades;
  * Aplica: Herencia (extiende Empleado), Polimorfismo (sobreescribe getRol y getPermisosAcceso)
  */
 public class Administrador extends Empleado {
+
+    public Administrador(int idEmpleado, String nombre, String tipoDocumento, String numDocumento) {
+        super(idEmpleado, nombre, tipoDocumento, numDocumento);
+    }
     
-    public Administrador(int idEmpleado, String nombre, String cedula) {
-          super(idEmpleado, nombre, cedula);
-  }
+   
     
-  @Override
+    @Override
   public String getRol() {
-      
-      return "administrador";
+  return "administrador";
+  
   }
   
    @Override
   public String[] getPermisosAcceso() {
-   String[] permisos = {
-     "GESTIONAR_PRODUCTOS",
-     "GESTIONAR_EMPLEADOS",
-     "VER_INVENTARIO",
-     "REGISTRAR_VENTA",
-     "VER_HISTORIAL_VENTAS",
-     "ACTUALIZAR_INVENTARIO"
+  String[] permisos = {
+  "GESTIONAR_PRODUCTOS",
+  "GESTIONAR_EMPLEADOS",
+  "VER_INVENTARIO",
+  "REGISTRAR_VENTA",
+  "VER_HISTORIAL_VENTAS",
+  "ACTUALIZAR_INVENTARIO"
   };
-   
-      return permisos;
+  return permisos;
  }
 
     @Override
     public String mostrarInfo() {
      return "Id Empleado: " + getIdEmpleado() +  "\n"
             + "Nombre: " + getNombre() +  "\n"
-            + "Cedula: " + getCedula();
+            + "Tipo documento: " + getTipoDocumento() +  "\n" 
+            + "Numero Documento: " + getNumDocumento();
     }
 }
