50a45b686a9ae50ee731a1b9e9dea89a0b27a669
mnahiun <duartemateusnahin701@gmail.com>
2026-05-20 20:54:20 -0500

    feat: Se creo la clase RolControl

---
+++ src/negocio/RolControl.java
@@ -0,0 +1,136 @@
+
+package negocio;
+
+import datos.RolDAO;
+import entidades.Rol;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.table.DefaultTableModel;
+
+
+public class RolControl {
+
+    
+    private final RolDAO DATOS;
+    private Rol obj;
+    private DefaultTableModel modeloTabla;
+    public int registrosMostrados;
+    
+    public RolControl() {
+        this.DATOS = new RolDAO();
+        this.obj = new Rol();
+        this.registrosMostrados = 0;
+        
+        
+    }
+    
+     public DefaultTableModel listar(String texto){
+     List<Rol> lista = new ArrayList<>();
+     lista.addAll(DATOS.listar(texto));
+
+     String[] titulos = {"Id",  "Nombre",  "Descripcion",  "Estado"};
+     this.modeloTabla = new DefaultTableModel(null, titulos);
+
+     String estado;
+     String[] registro =  new String[4];
+     this.registrosMostrados=0;
+     
+
+     for(Rol item:lista){
+        if(item.isActivo()){
+            
+        estado = "Activo";
+
+        }else{
+            estado = "Inactivo";
+        }
+
+        
+        registro [0] = Integer.toString(item.getId());
+        registro [1] = item.getNombre();
+        registro [2] = item.getDescripcion();    
+        registro [3] = estado;
+        this.modeloTabla.addRow(registro);
+        this.registrosMostrados=this.registrosMostrados+1;
+    
+     }
+       return this.modeloTabla;
+    }
+
+    public String insertar( String nombre,String descripcion){
+    if(DATOS.existe(nombre)){
+        return "El registro ya existe";
+    }else{
+        obj.setNombre(nombre);
+        obj.setDescripcion(descripcion);
+       
+        if(DATOS.insertar(obj)){
+            return "OK";
+        }else{
+            return "Error en el registro";
+        }
+    }
+    }
+
+    public String actualizar(int id, String nombre, String nombreAnt, String descripcion){
+        if (nombre.equals(nombreAnt)) {
+            obj.setId(id);
+            obj.setNombre(nombre);
+            obj.setDescripcion(descripcion);
+            if (DATOS.actualizar(obj)) {
+                return "OK";
+            }else{
+                return "Error en la actualización";
+                
+                }
+        }else {
+            if (DATOS.existe(nombre)) {
+                return "El usuario ya existe";
+            }else{
+               obj.setId(id);
+               obj.setNombre(nombre);
+               obj.setDescripcion(descripcion);
+                if (DATOS.actualizar(obj)) {
+                    return "OK";
+                    
+                }else{
+                 return "Error en la actualización";              
+                }
+            }
+        }
+    }
+
+    public String desactivar(int id){
+        if (DATOS.desactivar(id)) {
+            return "OK";
+        }else{
+            return "No se puede desactivar el registro";
+        }
+    }
+
+    public String activar(int id){
+         if (DATOS.activar(id)) {
+            return "OK";
+        }else{
+            return "No se puede activar el registro";
+        }
+
+    }
+    
+    public String eliminar(int id){
+        if(DATOS.eliminar(id)){
+           return "OK";
+      }else{
+          return "No se puede eliminar  el registro";
+    }
+  }
+    
+     public int total(){
+        return DATOS.total();
+    }
+    
+    public int totalMostrados(){
+        return this.registrosMostrados;
+        
+    }
+}
