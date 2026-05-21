4d82f791f8c3f9f57d88198586a30044eed3e027
mnahiun <duartemateusnahin701@gmail.com>
2026-05-20 20:43:01 -0500

    feat: Se creo la clase Bodeguero

---
+++ src/entidades/Bodeguero.java
@@ -0,0 +1,29 @@
+
+package entidades;
+
+public class Bodeguero  extends Empleado{
+
+    public Bodeguero(int idEmpleado, String nombre, String tipoDocumento, String numDocumento) {
+        super(idEmpleado, nombre, tipoDocumento, numDocumento);
+    }
+    
+ 
+  
+    @Override
+  public String getRol() {
+  return "Bodeguero";
+  }
+  
+   @Override
+  public String[] getPermisosAcceso() {
+  String[] permisos = {"VER_INVENTARIO", "ACTUALIZAR_INVENTARIO"};
+  return permisos;
+  }
+  
+   @Override
+    public String mostrarInfo() {
+    return "Id empleado: " + getIdEmpleado() + "\n"
+          + "Tipo documento: " + getTipoDocumento() +  "\n" 
+          + "Numero Documento: " + getNumDocumento();
+    }
+}
