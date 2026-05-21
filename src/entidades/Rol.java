da468387e10dd38d498d2275a014054998825dd3
mnahiun <duartemateusnahin701@gmail.com>
2026-05-20 21:49:43 -0500

    feat: Se creo la clase Rol

---
+++ src/entidades/Rol.java
@@ -0,0 +1,102 @@
+
+package entidades;
+
+import java.util.Objects;
+
+public class Rol {
+    private int id;
+    private String nombre;
+    private String descripcion;
+    private boolean activo;
+
+    public Rol() {
+    }
+
+    public Rol(int id, String nombre, String descripcion, boolean activo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.activo = activo;
+    }
+
+    public Rol(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    
+    @Override
+    public String toString() {
+        return nombre;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + this.id;
+        hash = 97 * hash + Objects.hashCode(this.nombre);
+        hash = 97 * hash + Objects.hashCode(this.descripcion);
+        hash = 97 * hash + (this.activo ? 1 : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Rol other = (Rol) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (this.activo != other.activo) {
+            return false;
+        }
+        if (!Objects.equals(this.nombre, other.nombre)) {
+            return false;
+        }
+        return Objects.equals(this.descripcion, other.descripcion);
+    }
+
+   
+    
+    
+}
